829c3a5f587b5f1b86de968e5044254391c9bc33
louise <louise.bc@aluno.ifsc.edu.br>
2026-06-09 20:21:52 -0300

    Desenhando baralho inteiro

--- aula-19/src/main/java/poo/App.java
+++ aula-19/src/main/java/poo/App.java
@@ -3,11 +3,16 @@ import edu.princeton.cs.algs4.Draw;
 import edu.princeton.cs.algs4.DrawListener;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.function.ToDoubleBiFunction;
 
 public class App implements DrawListener {
 
 
     private Draw draw;
+    private ArrayList<CartaGrafica> baralho = new ArrayList<>();
+
+
     CartaGrafica asO = new CartaGrafica(Naipe.OURO, Valor.AS, 100, 100);
 
 
@@ -21,17 +26,34 @@ public class App implements DrawListener {
         this.draw.enableDoubleBuffering();
         this.draw.addListener(this);
 
+        //TODO ARRUMAR ESSE TROÇO
+        int x =10;
+        int y = 600;
+        for (Naipe n : Naipe.values()){
+            for (Valor v : Valor.values()){
+                this.baralho.add(new CartaGrafica(n, v, x, y));
+                x+= 100;
+            }
+            x = 10;
+            y-=60;
 
-        asO.desenhar(draw);
+        }
+
+        for(CartaGrafica c: baralho){
+            c.desenhar(this.draw);
+        }
+
     }
 
     @Override
     public void mouseClicked(double x, double y) {
-        if(asO.clicouDentro(x, y)){
-            asO.desenhar(draw);
+
+        for(CartaGrafica c: baralho){
+            if (c.clicouDentro(x, y)){
+                c.desenhar(this.draw);
+            }
+
         }
-
-
     }
 
     static void main (String[] args){
